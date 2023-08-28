6d1d38737d2e60ae04bc292d88762f5efe77517b
sys08001 <117056205+sys08001@users.noreply.github.com>
2023-08-28 19:17:25 -0400

    Create a 16x16 grid

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 let gridContainer = document.querySelector('#grid-container');
 
-for (let i = 0; i < 4; i++) {
+for (let i = 0; i < 16; i++) {
     let gridRow = document.createElement('div');
     gridRow.classList.add('grid-row');
     gridContainer.appendChild(gridRow);
-    for (let j = 0; j < 4; j++) {
+    for (let j = 0; j < 16; j++) {
         let gridItem = document.createElement('div');
         gridItem.classList.add('grid-item');
         gridRow.appendChild(gridItem);
