579cd37dbede74336b53b9e9143bf93315053551
sys08001 <117056205+sys08001@users.noreply.github.com>
2023-08-29 14:38:33 -0400

    Add hover effect for squares and allow user to enter a custom size

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
 let gridContainer = document.querySelector('#grid-container');
+let button = document.querySelector('button');
 
-for (let i = 0; i < 16; i++) {
-    let gridRow = document.createElement('div');
-    gridRow.classList.add('grid-row');
-    gridContainer.appendChild(gridRow);
-    for (let j = 0; j < 16; j++) {
-        let gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridRow.appendChild(gridItem);
+// Draw the initial 16x16 grid on page load
+drawGrid();
+
+// Prompt the user to enter amount of squares per side when clicking button
+// Max amount is capped to 100 for performance reasons
+button.addEventListener('click', (e) => {
+    let userSize = prompt('Enter an amount of squares (per side)');
+    if (userSize >= 100) {
+        userSize = 100;
+    }
+    // Remove the old grid before drawing the new one with a custom size
+    gridContainer.innerHTML = '';
+    drawGrid(userSize);
+})
+
+// Draws a grid of either:
+// 1) The default of 16x16
+// 2) A user provided value (per side)
+function drawGrid(size = 16) {
+    for (let i = 0; i < size; i++) {
+        let gridRow = document.createElement('div');
+        gridRow.classList.add('grid-row');
+        gridContainer.appendChild(gridRow);
+        for (let j = 0; j < size; j++) {
+            let gridItem = document.createElement('div');
+            gridItem.classList.add('grid-item');
+            // Changes the color of a square on mouse hover
+            gridItem.addEventListener('mouseover', (e) => {
+                e.target.classList.add('grid-item-hover');
+            });
+            gridRow.appendChild(gridItem);
+        }
     }
 }
